4b0471c14144fa47455bcf2a7d019d332006f2ec
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-10-22 06:33:12 -0300

    creating the route to get token access from github

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,15 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
 
+import { router } from './routes';
+
 const api = express();
 
 api.use(express.json());
+api.use(router);
 
 api.get("/github", (request, response) => {
   response.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.
     GITHUB_CLIENT_ID}`
   );
+});
+
+api.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+  response.json(code);
 })
 
 api.listen(3333, () => console.log('Server is running on PORT 3333 🚀'));
