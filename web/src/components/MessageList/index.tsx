857f261cc13a5cfebf76bea917e04cd7b70d1ab3
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-10-29 06:25:49 -0300

    creation of components, styles and connection with backend

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,54 @@
+import { useEffect } from 'react'
+
+import { api } from '../../services/api'
+
+import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList() {
-  return  (
-    <h1>MessageList</h1>
+  useEffect(() => {
+    api.get('/messages/last3').then(response => {
+      console.log(response.data);
+    })
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/fabianocsouza.png" alt="Fabiano Souza" />
+            </div>
+            <span>Fabiano Souza</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/fabianocsouza.png" alt="Fabiano Souza" />
+            </div>
+            <span>Fabiano Souza</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/fabianocsouza.png" alt="Fabiano Souza" />
+            </div>
+            <span>Fabiano Souza</span>
+          </div>
+        </li>
+      </ul>
+
+
+    </div>
   )
 }
