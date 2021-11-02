0be4ecac22af81b023966aea259b14deea0976a0
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-11-02 18:05:09 -0300

    creating authentication, and the login, message and form components

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 import { api } from '../../services/api'
 
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
+
 interface Message {
   id: string;
   text: string;
@@ -12,8 +14,31 @@ interface Message {
     name: string;
   }
 }
+
+const messageQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messageQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messageQueue.length > 0){
+        setMessages(prevState => [
+          messageQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messageQueue.shift();
+      }
+    }, 3000)
+  }, []);
 
   useEffect(() => {
     api.get('/messages/last3')
--- web/src/contexts/auth.tsx
+++ web/src/contexts/auth.tsx
@@ -11,6 +11,7 @@ type User = {
 type AuthContextData = {
   user: User | null;
   signInUrl: string;
+  signOut: () => void;
 }
 export const AuthContext = createContext({} as AuthContextData);
 
@@ -40,8 +41,14 @@ export function AuthProvider(props: AuthProvider) {
     const { token, user } = response.data;
 
     localStorage.setItem('@dowhile:token', token);
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
 
     setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem('@dowhile:token');
   }
 
   useEffect(()=>{
@@ -69,7 +76,7 @@ export function AuthProvider(props: AuthProvider) {
   },[]);
   
   return (
-    <AuthContext.Provider value={{signInUrl, user}}>
+    <AuthContext.Provider value={{signInUrl, user, signOut}}>
       {props.children}
     </AuthContext.Provider>
   );
