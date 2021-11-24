2fc8955cf1360b7ed7237fd03c3214ba8468e69d
Fabiano Cardozo <fabiano1403@hotmail.com>
2021-11-24 07:12:13 -0300

    creating dynamic button

---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TouchableOpacity, TouchableOpacityProps, Text, ColorValue } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'
+
+import { styles } from './styles';
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  color: ColorValue;
+  backgroundColor: ColorValue;
+  icon?: React.ComponentProps<typeof AntDesign>['name']
+}
+
+export function Button({ title, color, backgroundColor, icon, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor }]}
+      {...rest}
+    >
+      <AntDesign  
+        name={icon}  size={24} style={styles.icon}/>
+      <Text
+        style={[styles.title, { color }]}
+      >
+        { title }
+      </Text>
+    </TouchableOpacity>
+  )
+}
